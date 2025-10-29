5f784f7fa8cec374a466b7425f3e8238786ad0c4
Bryce Carson <bcars268@mtroyal.ca>
2025-10-29 01:02:29 -0600

    Significant progress towards deparsing.

--- test/test.el
+++ test/test.el
@@ -1,4 +1,14 @@
 (defvar tool (peg-noweb-parse-files "~/src/peg-noweb/src/peg-noweb.nw" "~/src/whyse/src/whyse.nw"))
-;; (length tool); number of files
-;; (cl-first (cl-first tool)); (caar tool); first file name.
-;; (cl-first (cl-second tool)); (caadr tool); second file name.
+(defvar no-weave "/home/bryce/src/noweb/src/shell/noweave.nw")
+(defvar no-weave-parsed (peg-noweb-parse-files no-weave))
+(defvar document (peg-noweb-document 0 no-weave-parsed))
+(defvar markup
+  (progn (call-process "/usr/local/noweb/lib/markup" nil
+                       (setq no-weave-buffer (create-file-buffer no-weave))
+                       nil no-weave)
+         (with-current-buffer no-weave-buffer
+           (buffer-string))))
+
+(with-current-buffer (get-buffer-create "deparse-buffer")
+  (insert (peg-noweb-document-deparse document)))
+(ediff-buffers no-weave-buffer (get-buffer-create "deparse-buffer"))
